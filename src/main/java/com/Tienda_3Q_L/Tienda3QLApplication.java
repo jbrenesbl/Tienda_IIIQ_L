5b31888f6099e70a6b4088384e3aacc1d4f01d5d
jbren <jbren@DESKTOP-P2ANOM0>
2022-09-12 20:43:35 -0600

    Creación del proyecto inicial

---
+++ src/main/java/com/Tienda_3Q_L/Tienda3QLApplication.java
@@ -0,0 +1,13 @@
+package com.Tienda_3Q_L;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tienda3QLApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tienda3QLApplication.class, args);
+	}
+
+}
